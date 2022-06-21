53846e6190c1926fc8bd4ad4c89c9a0a04ebb846
Devin Shende <devin.s.shende@gmail.com>
2022-06-21 03:06:11 -0700

    some more crags and attempt at scrolling

--- lib/pages/bookmarks.dart
+++ lib/pages/bookmarks.dart
@@ -10,19 +10,27 @@ class BookmarksPage extends StatelessWidget {
         title: Text("Bookmarks", style: headerText),
       ),
       backgroundColor: Colors.black12,
-      body: Container(
-        child: Center(
-          child: Column(
-            children: [
-              AreaBar("Rodellar - Mascun", 1300,
-                  'https://stock.patitucciphoto.com/img-get/I0000lcGtYRsNFBc/s/885/750/15CL0216.jpg'),
-              SizedBox(height: 15),
-              AreaBar("Terradets", 220,
-                  'https://bloximages.newyork1.vip.townnews.com/lancasteronline.com/content/tncms/assets/v3/editorial/6/6d/66d15cc2-c7e4-11eb-be9f-07dc2475fe9b/60bea5bc7a6c2.image.jpg?resize=1396%2C1002'),
-              SizedBox(height: 15),
-              AreaBar("Siurana", 2500,
-                  'https://awe365.com/wp-content/uploads/2020/04/Helga-Rodellar-rockbusters.net_.jpg'),
-            ],
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10.0),
+          child: Center(
+            child: Column(
+              // padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                AreaBar("Rodellar - Mascun", 1300,
+                    'https://stock.patitucciphoto.com/img-get/I0000lcGtYRsNFBc/s/885/750/15CL0216.jpg'),
+                SizedBox(height: 15),
+                AreaBar("Terradets", 220,
+                    'https://bloximages.newyork1.vip.townnews.com/lancasteronline.com/content/tncms/assets/v3/editorial/6/6d/66d15cc2-c7e4-11eb-be9f-07dc2475fe9b/60bea5bc7a6c2.image.jpg?resize=1396%2C1002'),
+                SizedBox(height: 15),
+                AreaBar("Siurana", 2500,
+                    'https://awe365.com/wp-content/uploads/2020/04/Helga-Rodellar-rockbusters.net_.jpg'),
+                SizedBox(height: 15),
+                AreaBar("Margalef", 1300,
+                    'https://gripped.com/wp-content/uploads/2015/03/ml.jpg'),
+                SizedBox(height: 15),
+              ],
+            ),
           ),
         ),
       ),
