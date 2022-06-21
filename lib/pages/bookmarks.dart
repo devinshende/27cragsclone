14d51c090777f6c52c3da80fb883d6f3783c8d95
Devin Shende <devin.s.shende@gmail.com>
2022-06-21 02:38:52 -0700

    bookmarks page start

--- lib/pages/bookmarks.dart
+++ lib/pages/bookmarks.dart
@@ -14,9 +14,14 @@ class BookmarksPage extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              AreaBar("Rodellar - Mascun"),
-              SizedBox(height: 30),
-              AreaBar("Terradets"),
+              AreaBar("Rodellar - Mascun", 1300,
+                  'https://stock.patitucciphoto.com/img-get/I0000lcGtYRsNFBc/s/885/750/15CL0216.jpg'),
+              SizedBox(height: 15),
+              AreaBar("Terradets", 220,
+                  'https://bloximages.newyork1.vip.townnews.com/lancasteronline.com/content/tncms/assets/v3/editorial/6/6d/66d15cc2-c7e4-11eb-be9f-07dc2475fe9b/60bea5bc7a6c2.image.jpg?resize=1396%2C1002'),
+              SizedBox(height: 15),
+              AreaBar("Siurana", 2500,
+                  'https://awe365.com/wp-content/uploads/2020/04/Helga-Rodellar-rockbusters.net_.jpg'),
             ],
           ),
         ),
@@ -26,9 +31,11 @@ class BookmarksPage extends StatelessWidget {
 }
 
 class AreaBar extends StatelessWidget {
-  const AreaBar(this.name);
+  const AreaBar(this.name, this.routeCount, this.url);
 
   final String name;
+  final int routeCount;
+  final String url;
 
   @override
   Widget build(BuildContext context) {
@@ -37,8 +44,7 @@ class AreaBar extends StatelessWidget {
       child: Row(
         children: [
           FittedBox(
-            child: Image.network(
-                'https://stock.patitucciphoto.com/img-get/I0000lcGtYRsNFBc/s/885/750/15CL0216.jpg'),
+            child: Image.network(url),
             fit: BoxFit.fill,
           ),
           Expanded(
@@ -53,7 +59,8 @@ class AreaBar extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(name, style: h3),
-                    Text("1220 routes", style: greyText),
+                    SizedBox(height: 10),
+                    Text("$routeCount routes", style: greyText),
                   ],
                 ),
               ),
